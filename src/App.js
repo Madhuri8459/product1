cddc7822aafb150cdfb8941ffdb49fe72b79893d
Madhuri8459 <131444689+Madhuri8459@users.noreply.github.com>
2023-05-31 16:32:23 +0530

    added product component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components.js/Navbar'
-
+import Product from './components.js/Product'
 
 const App = () => {
   return (
     <div>
      <Navbar/>
+     <Product/>
+    
     
       </div>
   )
---
+++ src/components.js/Product.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function product() {
+  return (
+    <>
+    <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Cafe Timing </h1>
+        <div class="h-1 w-20 bg-yellow-500 rounded"></div>
+      </div>
+     
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://b.zmtcdn.com/data/pictures/9/20272519/691db6d3fda9adb37d05f03af0ba8069.jpg?fit=around|750:500&crop=750:500;*,*" alt="content"/>
+          <button class="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://b.zmtcdn.com/data/pictures/6/19988826/327bbcaf6799953a78c67b438ed7cab2.jpg" alt="content"/>
+          <button class="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-100 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Bambayya_Pav_bhaji.jpg" alt="content"/>
+          
+    <button class="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+  
+        </div>
+      </div>
+      
+      
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
+
+export default product
